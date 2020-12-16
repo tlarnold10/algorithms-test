6a1aef44b58c36ddb9dd679040ac07a1dc98d3ad
tlarnold10 <tlarnold10@gmail.com>
2020-12-15 20:59:40 -0600

    fizzbuzz problem solved, suggested based on a book

---
+++ fizz-bizz.py
@@ -0,0 +1,14 @@
+# Write a short program that prints each number from 1 to 100 on a new line. 
+# For each multiple of 3, print "Fizz" instead of the number. 
+# For each multiple of 5, print "Buzz" instead of the number. 
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+for i in range(1,101):
+    if(i%3 == 0 and i%5 == 0 ):
+        print("FizzBuzz")
+    elif(i%3 == 0):
+        print("Fizz")
+    elif(i%5 == 0):
+        print("Buzz")
+    else:
+        print(i)
