64c3e0fd3b8bd5746f42b74a1eb6158fbee07067
tlarnold10 <tlarnold10@hotmail.com>
2021-01-30 08:05:59 -0600

    working on my data structures

---
+++ python-data-structures.py
@@ -0,0 +1,26 @@
+# arrays - done
+# linked lists - done
+# stacks
+# queues
+# hash tables
+# trees
+# heaps
+# graphs
+from collections import deque
+
+def fun_arrays():
+    print("This is my array function")
+    array_example = ["this", "is", "my", "array"]
+    print(type(array_example))
+    print(array_example)
+
+def linked_lists():
+    llist = deque(["hey", "would", "you", "cuddle", "me"])
+    print(llist)
+    llist.append("maybe?")
+    print(llist)
+    llist.pop()
+    print(llist)
+
+# fun_arrays()
+linked_lists()
